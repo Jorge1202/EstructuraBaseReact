33071f07bbe3b0d564ab5931a05bd63e4cdc5629
Jorge1202 <yorch.uaeh@gmail.com>
2022-06-02 20:53:20 -0500

    App Routes

---
+++ src/Container/Index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Index() {
+    return (
+        <div>
+            Esto es una prueba 2
+        </div>
+    );
+}
+
+export default Index;
--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,11 +1,24 @@
 import React from 'react';
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
 import '../assets/css/global.scss'
+
+import Index from '../Container/Index'
 
 const App = () => {
     return (
-        <div>
-            Esto es una prueba 2
-        </div>
+        <BrowserRouter>
+            <Routes>
+                <Route path={'/'} element={<Index/>}></Route>
+                {/* <Route path="/" element={<App />}>
+                    <Route index element={<Home />} />
+                    <Route path="teams" element={<Teams />}>
+                        <Route path=":teamId" element={<Team />} />
+                        <Route path="new" element={<NewTeamForm />} />
+                        <Route index element={<LeagueStandings />} />
+                    </Route>
+                </Route> */}
+            </Routes>
+        </BrowserRouter>
     );
 };
 
